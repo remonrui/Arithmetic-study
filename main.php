21768de2b0f2e88ca3fa3ae7c4bd9cf2a8f91d28
remon <15856943206@163.com>
2019-02-13 12:38:30 +0800

    堆排序

--- main.php
+++ main.php
@@ -13,11 +13,12 @@ require_once (MainPath.'/sort/insertSort.php');
 require_once (MainPath.'/sort/mergeSort.php');
 require_once (MainPath.'/sort/quickSort.php');
 require_once (MainPath.'/sort/selectionSort.php');
+require_once (MainPath.'/sort/heapSort.php');
 
 
 function main()
 {
-    $arr = generateRandomArray(10000,0,10);
+    $arr = generateRandomArray(10000,1,1000);
 
 //    dump($arr);
     TestSortTime("sort",$arr);
@@ -29,6 +30,7 @@ function main()
     TestSortTime("quickSort",$arr);
     TestSortTime("quickSort1",$arr);
     TestSortTime("quickSort2",$arr);
+    TestSortTime("heapSort",$arr);
 
 //    quickSort2($arr);
 
@@ -36,7 +38,7 @@ function main()
 //
 //    quickSort($arr);
 //
-//    dump($arr);
+    heapSort($arr);
 
 }
 
